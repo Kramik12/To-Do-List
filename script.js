9b005af11193ffb75651f0913d025fbfcd42786e
Kramik <kramikkafle7@gmail.com>
2024-05-24 15:21:53 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if(inputBox.value === '') {
+        alert("YOu must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
